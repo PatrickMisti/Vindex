2ba074fec666e0904708f85a26d6eb09b2dfa3c2
Patrick <patrickmistlberger@gmial.com>
2020-12-31 17:56:37 +0100

    implement overview

---
+++ Vindex/lib/overview.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Overview extends StatefulWidget {
+  @override
+  _Overview createState() => _Overview();
+}
+
+class _Overview extends State<Overview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
